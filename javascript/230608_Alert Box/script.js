8ad25a4c19276430bf5f4a1016c73b57a437975a
jieun0212 <wldnjs4983@naver.com>
2023-06-08 16:50:13 +0900

    💄 alert box 스타일링

--- javascript/230608_Alert Box/script.js
+++ javascript/230608_Alert Box/script.js
@@ -1,1 +1,10 @@
 // document.write('12121')
+document.getElementById('open-btn').addEventListener('click', function(){
+  document.getElementById('alert').style.display = 'flex';
+})
+
+function closeAlert(){
+  document.getElementById('alert').style.display = 'none';
+}
+
+document.getElementById('close-btn').addEventListener('click', closeAlert);
