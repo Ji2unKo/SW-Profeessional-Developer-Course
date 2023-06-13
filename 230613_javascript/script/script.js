07f3a501e8f3895c5fdf728ba2441b09b725c684
jieun0212 <wldnjs4983@naver.com>
2023-06-13 16:50:40 +0900

    🎉 Javascript alert

---
+++ 230613_javascript/script/script.js
@@ -0,0 +1,37 @@
+// document.getElementById("apple").onclick = function(){
+//   alert("누르지말랬잖아");
+// }
+
+// 윈도우 닫기 window.close();
+
+// 1. 자바스크립트는 컴파일이 필요없는 인터프리터 언어이다.
+// 2. 자바스크립트는 그 어떤 플랫폼이라도 브라우저만 있으면 정상 작동된다.
+
+// alert("안녕하세요\n\'저\'는 \"고지은\" 입니다.");
+// 이스케이프 문자열 \',"" \n 줄바꿈
+
+
+// var(variables) 만들 변수의 이름;
+// 문자열은 "" 꼭 쓰기
+// var aaa; 변수를 만들고
+// var aaa = "3"; // 변수에 값을 넣고
+// alert(aaa);  // 그 내용을 확인했다.
+// aaa; 불러올때 
+// int(interer) aaa; 
+
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+// alert(c);
+
+// var a = "안녕하세요";
+// var b = "hello";
+// var c = a+b;
+
+// alert(c);
+
+// arr=array []
+var arr = [20, 35, 40, 50];
+// 함수에서는 1번째 부터가 아니라 0번째 부턱 시작
+alert( arr[0] );
+// 4번째 사용하면 undefined 이 변수에 언젠가 객체를 할당하려고 하는데, 지금은 객체가 없습니다.
