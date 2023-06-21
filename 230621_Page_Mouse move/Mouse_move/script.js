55ec4b546f1b621dce875ff8ac60671aa38e98cf
jieun0212 <wldnjs4983@naver.com>
2023-06-21 16:21:56 +0900

    ✨ mouse move

--- 230621_Page_Mouse move/Mouse_move/script.js
+++ 230621_Page_Mouse move/Mouse_move/script.js
@@ -1,7 +1,12 @@
-const pointer = document.getElementById('pointer')
 // console.log(pointer)
 
 window.addEventListener('mousemove',(event) => {
+  const pointer = document.getElementById('pointer')
   // console.log('마우스 좌표x값: '+ event.clientX + '마우스 좌표 Y값: ' + event.clientY)
-  console.log(`마우스 X값 : ${event.clientX}, 마우스 Y값: ${event.clientY}`)
+  // console.log(`마우스 X값 : ${event.clientX}, 마우스 Y값: ${event.clientY}`)
+  pointer.style.top = `${event.clientY - (pointer.offsetHeight / 2)}px`
+  pointer.style.left = `${event.clientX - (pointer.offsetWidth / 2)}px`
+
+  // console.log(pointer.clientWidth)
+  console.log(pointer.offsetWidth)
 })
