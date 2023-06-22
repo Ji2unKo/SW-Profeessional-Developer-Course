3883bbb11bd15e8eb92ec0c601d3b972a10b2b32
jieun0212 <wldnjs4983@naver.com>
2023-06-22 14:42:10 +0900

    ♻️ const char

--- 230620_UI_header_navigation/navigation/script.js
+++ 230620_UI_header_navigation/navigation/script.js
@@ -3,11 +3,14 @@ const list = document.querySelector('.list')
 const listItem = Array.from(list.querySelectorAll('.list-item'))
 // console.log(listItem)
 
+// MDN 참고 문서 링크
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 listItem.forEach(item => {
-  item.addEventListener('click',()=> {
+  item.addEventListener('click',() => {    
     // 1. 일단 모든 요소에서 active 클래스를 삭제
     listItem.forEach(item => item.classList.remove('active'))
-    // 2.내가 현재 누른 버튼에 active 클래스를 추가
+  
+    // 2. 내가 현재 누른 버튼에 active 클래스를 추가
     item.classList.add('active')
   })
 })
@@ -15,11 +18,12 @@ listItem.forEach(item => {
 
 
 
-// const char = ['a','b','c','d']
 
+
+// const char = ['a','b','c','d']
 // i를 0,1,2,3 으로 증가시키는 for문
 // for(let i = 0; i < char.length; i++){
-// console.log('for문으로 돌린 결과'+char+[i])
+//   console.log('for문으로 돌린 결과'+char[i])
 // }
 
 // 배열 char안의 요소들의 길이에 맞게 알아서 증가시킨 값을 가져옴
@@ -28,5 +32,3 @@ listItem.forEach(item => {
 //   console.log("forEach로 돌린 결과" + item)
 //   console.log("forEach문의 index 값" + index)
 // })
-
-
