510d9332a37d3878674f0953ba9fc3b85bf2fde0
jieun0212 <wldnjs4983@naver.com>
2023-07-07 14:04:14 +0900

    ✨ translateX 좌표 이동, script 코드 작성

--- 230706_slide/slide1/script.js
+++ 230706_slide/slide1/script.js
@@ -1,1 +1,24 @@
-let = a
+let btn1 = document.querySelector('.btn1')
+let btn2 = document.querySelector('.btn2')
+let btn3 = document.querySelector('.btn3')
+// console.log(btn1, btn2, btn3)
+let slideList = document.querySelector('.slide-list')
+console.log(slideList)
+
+// 버튼 1 클릭 했을 때 이미지 (0) translateX()
+// addeEventListener('click')
+btn1.addEventListener('click', function(){
+  slideList.style.transform = 'translateX(0)'
+})
+
+
+// 버튼 2 클릭 했을 때 이미지 (0) translateX()
+btn2.addEventListener('click', function(){
+  slideList.style.transform = 'translateX(-100vw)'
+})
+
+// 버튼 3 클릭 했을 때 이미지 (0) translateX()
+btn3.addEventListener('click', function(){
+  slideList.style.transform = 'translateX(-200vw)'
+})
+
