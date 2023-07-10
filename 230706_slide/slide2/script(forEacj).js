5a53a7d8bddfd764216a60d4e8ec02a6d4aa7134
jieun0212 <wldnjs4983@naver.com>
2023-07-10 13:36:56 +0900

    ✨ 조건문으로 button active 클래스 추가/제거

--- 230706_slide/slide2/script(forEacj).js
+++ 230706_slide/slide2/script(forEacj).js
@@ -7,10 +7,21 @@ buttons.forEach(function(btn, index){
     let xValue = -index * 100 + 'vw';
 
     slideList.style.transform = `translateX(${xValue})`;
-    slideList.style.backgroundColor = 'dodgerblue'
+
+    // buttons라는 배열을 한 번 더 돌면서 모든 버튼이 가지고 있는 active class remove
+    // buttons.forEach(function(otherBtn){
+    //   otherBtn.classList.remove('active')
+    // })
+    // 클릭된 버튼에만 active class add
+    btn.classList.add('active')
+
+    // 조건문
+    // 현재 클릭외어 있는 버튼이 아닐 때 && active라는 class를 포함하고 있다면
+    buttons.forEach(function(otherBtn){
+      if(otherBtn !== btn && otherBtn.classList.contains('active')){
+        otherBtn.classList.remove('active');
+      }
+    })
   })
 })
 
-buttons.addEventListener("mouseover", function(e){
-  e.target.style.backgroundColor = 'dogerblue'
-}, )
