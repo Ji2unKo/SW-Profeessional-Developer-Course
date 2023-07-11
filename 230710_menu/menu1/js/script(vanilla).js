b303d8c892e0ddb08e3115657864a9edba840f38
jieun0212 <wldnjs4983@naver.com>
2023-07-11 15:26:45 +0900

    ✨ menu1 Vanilla js 구현

--- 230710_menu/menu1/js/script(vanilla).js
+++ 230710_menu/menu1/js/script(vanilla).js
@@ -1,7 +1,15 @@
-// let gnb = document.querySelector('.gnb')
-// let sub = document.querySelector('.sub')
-// console.log(sub)
+const menuItems = document.querySelectorAll('.gnb > li')
+// console.log(menuItems)
+menuItems.forEach(function(item){
+  const subMenu = item.querySelector('.sub');
+  item.addEventListener('mouseover', function(){
+    subMenu.classList.add('active');
+    this.querySelector('a').classList.add('on');
+  })
 
-// gnb.addEventListener('.click',function(){
-//   this
-// })
+  // mouseleave
+  item.addEventListener('mouseleave', function(){
+    subMenu.classList.remove('active');
+    this.querySelector('a').classList.remove('on')
+  })
+})
