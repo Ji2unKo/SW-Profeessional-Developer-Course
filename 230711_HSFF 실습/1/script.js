1ebf1ffff248430df35c2745588f8c2f47e6ec09
jieun0212 <wldnjs4983@naver.com>
2023-07-14 14:12:59 +0900

    🐛 hsff

--- 230711_HSFF 실습/1/script.js
+++ 230711_HSFF 실습/1/script.js
@@ -1,18 +1,33 @@
-let prev = document.querySelector('.prev')
-let next = document.querySelector('.next')
-let last = document.querySelector('.last')
+// jQuery
+// $('.gnb-main-item').hover(function(){
+//   // gnb-main-item에 hover했을 때,
+//   // .sub class를 찾아서 slideDown()
+//   $(this).find('.sub').stop().slideDown(300);
+//   $(this).children('a').addClass('active');
+// }, function(){
+//   $(this).find('.sub').stop().slideUp(300);
+//   $(this).children('a').removeClass('active');
+// })
 
-let slideList = document.querySelector('.slide-list')
-// console.log(slideList)
 
-prev.addEventListener('click', function(){
-  slideList.style.transform = 'translateX(0)'
-})
+// Vanilla JS
+let gnbItems = document.querySelectorAll('.gnb-main-item');
 
-next.addEventListener('click', function(){
-  slideList.style.transform = 'translateX(-100vw)'
-})
+gnbItems.forEach(function(item){
+  let subMenu = item.querySelector('.sub');
 
-last.addEventListener('click', function(){
-  slideList.style.transform = 'translateX(-200vw)'
+  item.addEventListener('mouseover', function(){
+    // sub 메뉴가 보이게 max-height : 265px
+    subMenu.classList.add('active');
+    // gnb-main-item a 태그에 on class 추가시 메인메뉴 바뀐 컬러 유지 (hover)
+    this.querySelector('a').classList.add('on');
+    this.querySelector('span').classList.add('is-active')
+  })
+
+  item.addEventListener('mouseleave', function(){
+    // sub 메뉴를 보이지 않게 max-heigt : 0
+    subMenu.classList.remove('active');
+    this.querySelector('a').classList.remove('on');
+    this.querySelector('span').classList.remove('is-active')
+  })
 })
