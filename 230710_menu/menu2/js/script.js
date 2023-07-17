7185c36b50e3e9d6ff9d308376b2f56c77d59a36
jieun0212 <wldnjs4983@naver.com>
2023-07-17 13:46:49 +0900

    ✨ menu2 on class add & remove

--- 230710_menu/menu2/js/script.js
+++ 230710_menu/menu2/js/script.js
@@ -5,3 +5,15 @@ $('.main-menu li, .submenu-wrapper').hover(function(){
   // leave 됐을 때 실행 할 함수
   $('.submenu-wrapper').stop().slideUp(500);
 })
+
+$('.submenu-list > li').hover(function(){
+  // .submenu-list > li 4개 중 hover된
+  // 자신의 index 번호를 i라는 변수에 저장
+  let i = $(this).index();
+  // console.log(i)
+  // eq() : index 번호로 어떤 요소를 찾을 때 사용
+  $('.main-menu').find('a').eq(i).addClass('on')
+}, function(){
+  let i = $(this).index();
+  $('.main-menu').find('a').eq(i).removeClass('on')
+})
