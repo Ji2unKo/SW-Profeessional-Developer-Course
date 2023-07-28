12821f39c2b8e074e87a4b1ed6d92d26b5e3e894
jieun0212 <wldnjs4983@naver.com>
2023-07-28 15:09:16 +0900

    📝

--- 230726_네이버회원가입/js/script.js
+++ 230726_네이버회원가입/js/script.js
@@ -9,3 +9,40 @@ $('label').on('click', function(e){
 }
 })
 
+// .total을 체크하면 .agree 올 체크
+// .total을 언체크하면 .agree 전부 언체크
+
+// .total .checkbox-img가 .checked 클래스를 가지고 있다면
+// .agree안 .checkbox-img에 checked class add
+// .agree안 input checkbox를 checked true
+
+// .total .checkbox-img가 .checked 클래스를 가지고 있지 않다면
+// .agree 안 .checkbox-img에 checked class remove
+// .agree 안 input checkbox를 checked false
+$('.total label').on('click', function(){
+  if($(this).children('.checkbox-img').hasClass('checked')) {
+    $('.agree').find('.checkbox-img').addClass('checked');
+  } else {
+    $('.agree').find('.checkbox-img').removeClass('checked')
+    $('.agree').find('input[type="checkbox"]').removeAttr('checked')
+  }
+})
+
+$('.agree label').on('click', function(){
+  let len = $('.agre')
+})
+
+
+// 조건이 참이라면
+//
+
+if(unchk == 0){
+
+}
+
+
+//
+// 하나라도 체크하지 않은 겡 있다면 submit() 을 막고
+
+// .req-alert 텍스트를 보여준다.
+
